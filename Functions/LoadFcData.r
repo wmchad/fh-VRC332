fa78ce32341bc0b665703149742a848f8d2b826d
Chad Young <wyoung@fhcrc.org>
2016-11-07 15:24:09 -0800

    Get only Fc data instead of also ICS data

--- Functions/LoadFcData.r
+++ Functions/LoadFcData.r
@@ -1,13 +1,15 @@
 require(dplyr)
 require(tidyr)
 setwd("~/Projects/VRC332/Data")
-fcData = read.table("longAnimalData2.narm.txt", header=TRUE)
 predSummary = read.table("predSummary.txt", header=TRUE)
 predSummary <- predSummary %>%
     filter(substr(varName, 1, 1)=="X") %>%
     separate(varName, c("tp", "re", "ag"), "_") %>%
     mutate(tp=substr(tp, 2, 2))
-
+fcData <- read.table("longAnimalData2.narm.txt", header=TRUE)
+fcData <- cbind(fcData[,1:7], fcData %>%
+                              select(one_of(as.character(predSummary$shortVarName))))
+fcData[,-(1:7)] <- ReplaceMissingWithMean(fcData[,-(1:7)])
 
 tpLabels <- c("Baseline", "1xDna", "2xDna", "PeakPostPrime",
               "PreBoost", "PeakPostBoost", "PreChallenge",
