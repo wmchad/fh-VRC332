07c916bfc7fdf18b23aa49c02c0353b472941ea8
Chad Young <wyoung@fhcrc.org>
2016-11-07 15:01:24 -0800

    Adding sample script, updating general functions

---
+++ Functions/RunRandomPartitionPredictions.r
@@ -0,0 +1,69 @@
+require(caret)
+
+fnFolder <- "~/Projects/VRC332/Code/fh-vrc332/Functions"
+source(file.path(fnFolder, "GetRmse.r"))
+
+RunRandomPartitionPredictions <- function(data, pTrain=0.75,
+                                          nRand=50, method="glmnet",
+                                          ctrl=trainControl(method="repeatedcv", repeats=5),
+                                          outdir="",
+                                          rmseFile="rmseDefault.txt",
+                                          fitsFile="fitsDefault.rdata",
+                                          verbose=TRUE, progressEvery=5) {
+  vtimestamp <- function() {}
+  vprint <- function(...) {}
+  if ( verbose ) {
+    vtimestamp <- function(){timestamp()}
+    vprint <- function(..., sep=""){print(paste(..., sep=sep))}
+  }
+
+  curdir <- getwd()
+  compRmse <- matrix(nrow=nRand, ncol=3)
+  colnames(compRmse) <- c("baseline", "group", "predicted")
+  fits <- NULL
+
+  vtimestamp()
+  vprint("Starting...")
+  for ( i in 1:nRand ) {
+    inTrain = createDataPartition(data$groups, p=0.75, list=FALSE)
+    train.x <- data$x[inTrain,]
+    train.y <- data$y[inTrain]
+    test.x <- data$x[-inTrain,]
+    test.y <- data$y[-inTrain]
+
+    fit <- GetRmse(train.x, train.y, test.x, test.y, method, ctrl)
+    fits[[i]] <- list(fit=fit$fit, predicted=fit$predicted, actual=test.y,
+                      testAnimals=data$animalIds[-inTrain])
+    compRmse[i,] <- c(RMSE(mean(train.y), test.y),
+                      GroupRmse(train.x, train.y, test.x, test.y),
+                      fit$rmse)
+    if ( i %% progressEvery == 0 ) {
+      vtimestamp()
+      vprint(paste(i, "iterations finished"))
+      setwd(outdir)
+      write.table(compRmse, rmseFile,
+                  quote=FALSE, row.names=FALSE, col.names=FALSE)
+      save(fits, file=fitsFile)
+    }
+  }
+  setwd(outdir)
+  write.table(compRmse, rmseFile,
+              quote=FALSE, row.names=FALSE, col.names=FALSE)
+  save(fits, file=fitsFile)
+  setwd(curdir)
+  list(compRmse=compRmse,
+       fits=fits)
+}
+
+GetRandomPartitionPredictions <- function(outdir="",
+                                          rmseFile="rmseDefault.txt",
+                                          fitsFile="fitsDefault.rdata") {
+  curdir <- getwd()
+  setwd(outdir)
+  compRmse <- read.table(rmseFile)
+  names(compRmse) <- c("baseline", "group", "predicted")
+  load(fitsFile)
+  setwd(curdir)
+  list(compRmse=compRmse,
+       fits=fits)
+}
---
+++ scripts/predict/SetpointVL/PredictByTimepoint.r
@@ -0,0 +1,38 @@
+require(caret)
+require(glmnet)
+require(reshape2)
+
+fnFolder <- "~/Projects/VRC332/Code/fh-vrc332/Functions"
+
+source(file.path(fnFolder, "LoadFcData.r"))
+source(file.path(fnFolder, "GetTimepointData.r"))
+source(file.path(fnFolder, "GetBestPredictorsData.r"))
+source(file.path(fnFolder, "ReplaceMissingWithMean.r"))
+source(file.path(fnFolder, "RunRandomPartitionPredictions.r"))
+
+nRand <- 50
+pTrain <- 0.75
+ctrl <- trainControl(method="repeatedcv", repeats=20)
+outputFolder <- "~/Projects/VRC332/Data/PredictSetpointVL"
+for ( tp in 0:8 ) {
+  cat("    Starting Timepoint", tp, "\n")
+  tpData <- GetTimepointData(vlData, tp, predSummary, TRUE, "LogSetpointVL")
+  tpData$x <- ReplaceMissingWithMean(tpData$x)
+  outdir <- file.path(outputFolder, paste("tp", tp, "-", tpLevels[tp+1], sep=""))
+  dir.create(outdir, showWarnings=FALSE, recursive=TRUE)
+  predResults <- RunRandomPartitionPredictions(
+                   tpData, pTrain=pTrain, nRand=nRand, method="glmnet", ctrl=ctrl,
+                   outdir=outdir,
+                   rmseFile=paste("tp", tp, "-", tpLevels[tp+1], "-CompRmse.txt", sep=""),
+                   fitsFile=paste("tp", tp, "-", tpLevels[tp+1], "-Fits.rdata", sep=""),
+                   verbose=TRUE, progressEvery=10)
+
+  bestPredData <- GetBestPredictorsData(vlData, predResults, nRand/2, TRUE, "LogSetpointVL")
+  bestPredData$x <- ReplaceMissingWithMean(bestPredData$x)
+  predResults <- RunRandomPartitionPredictions(
+                   bestPredData, pTrain=pTrain, nRand=nRand, method="glmnet", ctrl=ctrl,
+                   outdir=outdir,
+                   rmseFile=paste("tp", tp, "-", tpLevels[tp+1], "-BestPred-CompRmse.txt", sep=""),
+                   fitsFile=paste("tp", tp, "-", tpLevels[tp+1], "-BestPred-Fits.rdata", sep=""),
+                   verbose=TRUE, progressEvery=10)
+}
