9e7029ecbf5a3443576e14729e0ce6058f4e9173
Chad Young <wyoung@fhcrc.org>
2016-11-08 11:19:27 -0800

    Changed code to use dplyr more thoroughly

--- Functions/ConvertToLongData.r
+++ Functions/ConvertToLongData.r
@@ -5,8 +5,8 @@ ConvertToLongData <- function(fcData, predSummary) {
     longData <- melt(fcData[,-(3:4)],
                      id.vars=c("AnimalID2", "GroupNm", "NoChallenges",
                                "LogPeakVL", "LogSetpointVL"))
-    longData <- merge(longData,
-                      predSummary[,c("tp", "re", "ag", "shortVarName")],
-                      by.x="variable", by.y="shortVarName", all.x=TRUE)
+    longData <- left_join(longData,
+                      predSummary %>% select(tp:ag, shortVarName),
+                      by=c("variable"="shortVarName"))
     longData[,-1]
 }
